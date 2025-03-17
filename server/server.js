82fa3e9e00bb8f027d244fdf5b762e5928d0fa3f
Ritesh <itsmeriteshpatidar@gmail.com>
2025-03-17 09:57:48 +0530

    added hello

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import dbConnect from './config/dbConnect.js';
 import config from './config/config.js';
 
 const app = express();
-
+console.log('hello')
 dbConnect();
 
 app.listen(config.app.PORT, () => {
